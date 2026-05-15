51fe70f6d421c126e865723dcdc9da7f7d1dcba5
DeltaStudent-2002 <maheshdhondge26@gmail.ocm>
2026-05-15 19:51:38 +0530

    chore: initialize project with vite, tanstack

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+//  @ts-check
+
+/** @type {import('prettier').Config} */
+const config = {
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  arrowParens: "avoid",
+
+  plugin: ['prettier-plugin-tailwindcss'],
+};
+
+export default config;
